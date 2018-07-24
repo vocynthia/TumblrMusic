15887ce422c7a9ff7bcc4da464bafdd179558740
Cindy Vo <vocynthia2000@gmail.com>
2018-07-24 12:09:42 -0700

    IBoutletsAndIBactions

--- TumblrMusic/TumblrMusic/ViewControllers/ViewController.swift
+++ TumblrMusic/TumblrMusic/ViewControllers/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //IBOUTLETS
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var tagTextField: UITextField!
+    @IBOutlet weak var enterButton: UIButton!
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,17 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+//IBACTIONS
+    @IBAction func usernameTextFieldUsed(_ sender: Any) {
+    
+    }
+    
+    @IBAction func tagTextFieldUsed(_ sender: Any) {
+        
+    }
 
-
+    @IBAction func enterButtonPressed(_ sender: Any) {
+      print("lmao")
+    }
 }
 
