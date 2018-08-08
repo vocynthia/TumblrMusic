798ac1a17b4bd74b61d23f9e3e4e641540829bc3
Cindy Vo <vocynthia2000@gmail.com>
2018-08-08 10:49:51 -0700

    audioplayer works again with pause and play

--- TumblrMusic/ViewControllers/MusicListTableViewController.swift
+++ TumblrMusic/ViewControllers/MusicListTableViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 import Alamofire
 import SwiftyJSON
+import AVKit
+import MediaPlayer
 import AVFoundation
 import Kingfisher
 
@@ -19,14 +21,18 @@ enum State {
 
 class MusicListTableViewController: UITableViewController {
     
-    var trackIndex: Int!
+    @IBOutlet weak var playPauseButton: UIBarButtonItem!
+    var trackIndex: Int = 0
     var offset = 0
     var state = State.loaded
     
-    var toggleState = 1
+    var isPlaying = false
     var audioPlayer: AVPlayer!
     var username: String?
     var tag: String?
+    
+    var pausedTime: CMTime?
+    
     var viewControllerPost = [filter](){
         didSet{
             DispatchQueue.main.async {
@@ -35,52 +41,82 @@ class MusicListTableViewController: UITableViewController {
            
         }
     }
-    
-    func playSoundWith() -> Void {
+    func playSoundWith(c: Int ) -> Void {
 // if there is no audio
         if viewControllerPost.isEmpty {
             print("no audio found")
         } else {
-        let audioURL = URL(string: viewControllerPost[trackIndex].audioFile)
+            let audioURL = URL(string: viewControllerPost[c].audioFile)
         let playerItem = AVPlayerItem.init(url: audioURL!)
         audioPlayer = AVPlayer.init(playerItem: playerItem)
-       
-        
+        audioPlayer.automaticallyWaitsToMinimizeStalling = false
+        audioPlayer.playImmediately(atRate: 1.0)
         audioPlayer.play()
 
         }
     }
     
     
-    
+    /* TODO:
+    1. check if the image is playing and change it to pause when you select a cell
+     2. toggle state when you play a song form selection
+    */
     
     @IBAction func PlayPauseButton(_ sender: AnyObject) {
-            playSoundWith()
-            let playBtn = sender as! UIBarButtonItem
+//        print("time is: \(audioPlayer.currentTime().seconds)")
+//        playSoundWith()
+        let playBtn = sender as! UIBarButtonItem
         if viewControllerPost.isEmpty {
-        print("can't play lol")
-        } else if toggleState == 1 {
+            print("can't play lol")
+ 
+        } else if isPlaying == false {
+            
+            if audioPlayer == nil {
+                playSoundWith(c: 0)
+            }
+            
             // playback back to same time when audio was paused
             audioPlayer.automaticallyWaitsToMinimizeStalling = false
             audioPlayer.playImmediately(atRate: 1.0)
-            audioPlayer.currentTime()
-            audioPlayer.play()
-            toggleState = 2
+            isPlaying = true
             playBtn.image = UIImage(named:"pause.png")
+            
+            if pausedTime != nil {
+               audioPlayer.seek(to: pausedTime!)
+            }
+            
+            audioPlayer.play()
+            
+
         } else {
+            
+            pausedTime = audioPlayer.currentItem?.currentTime()
             audioPlayer.pause()
-            toggleState = 1
+            isPlaying = false
             
             playBtn.image = UIImage(named:"play.png")
         }
+//        if audioPlayer.pl {
+//    }
     }
     
     @IBAction func RewindButton(_ sender: Any) {
-    
+        trackIndex -= 1
+        playSoundWith(c: Int(trackIndex))
+        audioPlayer.play()
+     
+        
+        // SHOW SLECTED CELL
+        
+     
     }
     
     @IBAction func ForwardButton(_ sender: Any) {
-       
+        trackIndex += 1
+        tableView.selectRow(at: IndexPath.init(row: Int(trackIndex), section: 1 ), animated: true , scrollPosition: UITableViewScrollPosition(rawValue: 3)! )
+        playSoundWith(c: Int(trackIndex))
+        
+
     }
     
     
@@ -108,7 +144,20 @@ class MusicListTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         trackIndex = indexPath.row
-        playSoundWith()
+        if isPlaying == false {
+            // playback back to same time when audio was paused
+            playSoundWith(c: Int(trackIndex))
+            isPlaying = true
+            playPauseButton.image = UIImage(named:"pause.png")
+            
+        } else {
+            
+            pausedTime = audioPlayer.currentItem?.currentTime()
+            audioPlayer.pause()
+            isPlaying = false
+            playPauseButton.image = UIImage(named:"play.png")
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -137,6 +186,7 @@ class MusicListTableViewController: UITableViewController {
                         let audioObject = filter(json: audioPosts)
                         allAudioPosts.append(audioObject)
                         
+                        
                     }
                     self.viewControllerPost.append(contentsOf: allAudioPosts)
                     
@@ -153,12 +203,12 @@ class MusicListTableViewController: UITableViewController {
         
     }
     
-    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        if indexPath.row + 1 == viewControllerPost.count  && state != .loading {
-            doNetworkRequest()
-        }
-        
-    }
+//    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+//        if indexPath.row + 1 == viewControllerPost.count  && state != .loading {
+//            doNetworkRequest()
+//        }
+//        
+//    }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -177,6 +227,9 @@ class MusicListTableViewController: UITableViewController {
         // #warning Incomplete implementation, return the number of rows
         return viewControllerPost.count
     }
+   
+
+
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -189,9 +242,12 @@ class MusicListTableViewController: UITableViewController {
         
         cell.textLabel?.text = audio.trackName
         cell.detailTextLabel?.text = audio.artist
-//        cell.isSelected = Bool(audio.audioFile)!
         
+        if cell.isSelected {
+            cell.setHighlighted(true, animated: true)
+        }
         
+    
         if audio.albumArt.isEmpty{
             let image = UIImage(named: "album")
             cell.imageView?.image = image
@@ -212,6 +268,34 @@ class MusicListTableViewController: UITableViewController {
     }
         return cell
     }
+//    private func setupAVAudioSession() {
+//        do {
+//            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+//            try AVAudioSession.sharedInstance().setActive(true)
+//            debugPrint("AVAudioSession is Active and Category Playback is set")
+//            UIApplication.shared.beginReceivingRemoteControlEvents()
+//            setupCommandCenter()
+//        } catch {
+//            debugPrint("Error: \(error)")
+//        }
+//    }
+//    private func setupCommandCenter() {
+//        let audio = self.viewControllerPost
+//        MPNowPlayingInfoCenter.default().nowPlayingInfo = [MPMediaItemPropertyTitle: .trackName ]
+//
+//        let commandCenter = MPRemoteCommandCenter.shared()
+//        commandCenter.playCommand.isEnabled = true
+//        commandCenter.pauseCommand.isEnabled = true
+//        commandCenter.playCommand.addTarget { [weak self] (event) -> MPRemoteCommandHandlerStatus in
+//            self?.audioPlayer.play()
+//            return .success
+//        }
+//        commandCenter.pauseCommand.addTarget { [weak self] (event) -> MPRemoteCommandHandlerStatus in
+//            self?.audioPlayer.pause()
+//            return .success
+//        }
+//    }
+
 
     /*
     // Override to support conditional editing of the table view.
--- TumblrMusic/ViewControllers/ViewController.swift
+++ TumblrMusic/ViewControllers/ViewController.swift
@@ -13,7 +13,6 @@ import Alamofire
 import SwiftyJSON
 
 
-
 class ViewController: UIViewController {
     
     
